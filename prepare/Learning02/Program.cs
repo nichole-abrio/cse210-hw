901fb52b5d4d3f0d896a3021224ae4d2c04e086d
Nichole Abrio <ma.nichole.abrio00@gmail.com>
2023-01-13 03:15:33 +0400

    Resume added with display method

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,7 +9,32 @@ class Program
         job1._company = "Microsoft";
         job1._startYear = 2019;
         job1._endYear = 2022;
-        job1.Display();
+        //job1.Display();
+
+        //job2
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Microsoft";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        //job2.Display();
+
+        //job3
+        Job job3 = new Job();
+        job3._jobTitle = "Teacher";
+        job3._company = "Pola Central School";
+        job3._startYear = 2002;
+        job3._endYear = 2022;
+        //ob3.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
+
+        myResume.Display();
 
 
     }
